6974f5c216b785ebaee44b55c0fa704790ea566b
LadyJane1114 <sweet.jane.s@outlook.com>
2025-09-19 10:21:27 -0300

    Adding data models to Weather - work is incompete because I have to go to another class.

---
+++ app/src/main/java/com/example/redsky/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.redsky
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel(){
+    //insert code here
+}
--- app/src/main/java/com/example/redsky/models/Weather.kt
+++ app/src/main/java/com/example/redsky/models/Weather.kt
@@ -1,2 +1,14 @@
 package com.example.redsky.models
 
+data class Weather (
+    val current: Current,
+    val forecast: List<Forecast>
+)
+
+data class Current (
+    // insert code here
+)
+
+data class Forecast (
+    //insert code here
+)
